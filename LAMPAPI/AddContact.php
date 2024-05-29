c6dbab0e4c0ad9a4905fd89df91ac39ed059d260
Gage Notargiacomo <gagenotar@gmail.com>
2024-05-29 09:21:11 -0400

    Fixed PHP

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -5,15 +5,12 @@
     $serverUser = "TheBeast"; 
     $serverPass = "WeLoveCOP4331"; 
     $dbname = "ContactManager";
-	
-	$contact = $inData["contact"];
-	$userId = $inData["userId"];
 
 	$phoneNumber = $inData["phoneNumber"];
 	$emailAddress = $inData["emailAddress"];
 	$newFirst = $inData["newFirstName"];
 	$newLast = $inData["newLastName"];
-	$userId = $inData["userId"];
+	$userId = $inData["ID"];
 
     $conn = new mysqli($servername, $serverUser, $serverPass, $dbname);
 	if ($conn->connect_error) 
@@ -22,7 +19,7 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT INTO Contacts (FirstName, LastName, Phone, Email, UserID) VALUES(?,?,?,?,?)");
+		$stmt = $conn->prepare("INSERT INTO Contacts (FirstName, LastName, Phone, Email, ID) VALUES(?,?,?,?,?)");
 		$stmt->bind_param("sssss", $newFirst, $newLast, $phoneNumber, $emailAddress, $userId);
 		$stmt->execute();
 		$stmt->close();
