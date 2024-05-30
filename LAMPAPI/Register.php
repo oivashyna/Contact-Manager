e915e87d44b2219d6a380f8930c909aa091f43d6
Jason Devito <140463607+JT5016@users.noreply.github.com>
2024-05-30 10:25:02 -0400

    Update Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -2,47 +2,63 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    $inData = getRequestInfo();
 
-    $servername = "localhost"; 
-    $serverUser = "TheBeast"; 
-    $serverPass = "WeLoveCOP4331"; 
-    $dbname = "ContactManager";
-    
-    $firstName = $inData["firstName"];
-    $lastName = $inData["lastName"];
-    $login = $inData["login"];
-    $password = $inData["password"]; // Assuming password is already hashed
+$inData = getRequestInfo();
 
-    $conn = new mysqli($servername, $serverUser, $serverPass, $dbname);
-    if ($conn->connect_error) 
+$servername = "localhost"; 
+$serverUser = "TheBeast"; 
+$serverPass = "WeLoveCOP4331"; 
+$dbname = "ContactManager";
+
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$login = $inData["login"];
+$password = $inData["password"];
+
+$conn = new mysqli($servername, $serverUser, $serverPass, $dbname);
+
+if ($conn->connect_error) 
+{
+    returnWithError("Connection failed: " . $conn->connect_error);
+} 
+else 
+{
+    $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+    if ($stmt->execute())
     {
-        returnWithError($conn->connect_error);
-    } 
+        returnWithMessage("User registered successfully");
+    }
     else
     {
-        $stmt = $conn->prepare("INSERT INTO Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
-        $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
-        $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        returnWithError("");
+        returnWithError("Execute failed: " . $stmt->error);
     }
+    $stmt->close();
+    $conn->close();
+}
 
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-    function sendResultInfoAsJson($obj)
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo json_encode($obj);
+}
 
-    function returnWithError($err)
-    {
-        $retValue = '{"error":"' . $err . '"}';
-        sendResultInfoAsJson($retValue);
-    }
+function returnWithError($err)
+{
+    $retValue = ["error" => $err];
+    error_log(json_encode($retValue)); // Log the error
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithMessage($msg)
+{
+    $retValue = ["message" => $msg];
+    error_log(json_encode($retValue)); // Log the message
+    sendResultInfoAsJson($retValue);
+}
 ?>
