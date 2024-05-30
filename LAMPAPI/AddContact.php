c3edb8491ab03a697535eac09a4fe0a233e5288d
Jason Devito <140463607+JT5016@users.noreply.github.com>
2024-05-30 08:50:28 -0400

    Update AddContact.php

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,32 +1,31 @@
 <?php
-$inData = getRequestInfo();
+	$inData = getRequestInfo();
 
-$servername = "localhost"; 
-$serverUser = "TheBeast"; 
-$serverPass = "WeLoveCOP4331"; 
-$dbname = "ContactManager";
+    $servername = "localhost"; 
+    $serverUser = "TheBeast"; 
+    $serverPass = "WeLoveCOP4331"; 
+    $dbname = "ContactManager";
 
-$phoneNumber = $inData["phoneNumber"];
-$emailAddress = $inData["emailAddress"];
-$newFirstName = $inData["newFirstName"];
-$newLastName = $inData["newLastName"];
-$userID = $inData["userID"];
+	$phoneNumber = $inData["phoneNumber"];
+	$emailAddress = $inData["emailAddress"];
+	$newFirstName = $inData["newFirstName"];
+	$newLastName = $inData["newLastName"];
+	$userID = $inData["userID"];
 
-$conn = new mysqli($servername, $serverUser, $serverPass, $dbname);
-
-if ($conn->connect_error)
+    $conn = new mysqli($servername, $serverUser, $serverPass, $dbname);
+	if ($conn->connect_error) 
 	{
-			returnWithError( $conn->connect_error );
-	}
+		returnWithError( $conn->connect_error );
+	} 
 	else
 	{
-			$stmt = $conn->prepare("INSERT into Contacts (newFirstName,newLastName,newPhoneNumber,EmailAddress, UserID) VALUES(?,?,?,?,?)");
-			$stmt->bind_param("ssssi", $newFirstName, $newLastName, $phoneNumber, $emailAddress, $userID);
-			$stmt->execute();
-			$stmt->close();
-			$conn->close();
-			returnWithError("");
-		}
+		$stmt = $conn->prepare("INSERT INTO Contacts (FirstName, LastName, Phone, Email, UserId) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("ssssi", $newFirstName, $newLastName, $phoneNumber, $emailAddress, $userID);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
 
 	function getRequestInfo()
 	{
@@ -38,11 +37,11 @@ if ($conn->connect_error)
 		header('Content-type: application/json');
 		echo $obj;
 	}
-
+	
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-
+	
 ?>
