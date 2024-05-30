6171dfd92540cb872486c44230db71eba810e43e
Gage Notargiacomo <gagenotar@gmail.com>
2024-05-30 00:40:39 -0400

    Update

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,43 +1,70 @@
 <?php
-    $inData = getRequestInfo();
+$inData = getRequestInfo();
 
-    $servername = "localhost"; 
-    $serverUser = "TheBeast"; 
-    $serverPass = "WeLoveCOP4331"; 
-    $dbname = "ContactManager";
+$servername = "localhost"; 
+$serverUser = "TheBeast"; 
+$serverPass = "WeLoveCOP4331"; 
+$dbname = "ContactManager";
 
-    $ID = $inData["ID"];
-    $userID = $inData["userID"];
+$ID = $inData["ID"];
+$userID = $inData["userID"];
 
-    $conn = new mysqli($servername, $serverUser, $serverPass, $dbname);
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else 
+$conn = new mysqli($servername, $serverUser, $serverPass, $dbname);
+
+if ($conn->connect_error) 
+{
+    returnWithError($conn->connect_error);
+} 
+else 
+{
+    $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID = ? AND UserID = ?");
+    if ($stmt)
     {
-        $stmt = $conn->prepare("DELETE FROM Contacts where ID = ? and UserID = ?");
-        $stmt->bind_param("ss", $userID, $ID);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
+        $stmt->bind_param("ii", $ID, $userID);
+        if ($stmt->execute())
+        {
+            if ($stmt->affected_rows > 0)
+            {
+                returnWithMessage("Contact deleted successfully");
+            }
+            else
+            {
+                returnWithError("No contact found with the given ID and userID");
+            }
+        }
+        else
+        {
+            returnWithError($stmt->error);
+        }
+        $stmt->close();
     }
+    else
+    {
+        returnWithError($conn->error);
+    }
+    $conn->close();
+}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
 
+function returnWithError($err)
+{
+    $retValue = ["error" => $err];
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithMessage($msg)
+{
+    $retValue = ["message" => $msg];
+    sendResultInfoAsJson($retValue);
+}
 ?>
